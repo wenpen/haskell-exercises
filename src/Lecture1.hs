d218d1a542663d4d6be865d3e5e6a7b11b72783d
axp <645124937@qq.com>
2023-03-18 10:34:04 +0800

    lecture1

--- src/Lecture1.hs
+++ src/Lecture1.hs
@@ -32,13 +32,14 @@ module Lecture1
     ) where
 
 -- VVV If you need to import libraries, do it after this line ... VVV
-
+import Debug.Trace
 -- ^^^ and before this line. Otherwise the test suite might fail  ^^^
 
 {- | Specify the type signature of the following function. Think about
 its behaviour, possible types for the function arguments and write the
 type signature explicitly.
 -}
+makeSnippet :: Int -> String -> String
 makeSnippet limit text = take limit ("Description: " ++ text) ++ "..."
 
 {- | Implement a function that takes two numbers and finds sum of
@@ -54,7 +55,8 @@ Explanation: @sumOfSquares 3 4@ should be equal to @9 + 16@ and this
 is 25.
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-sumOfSquares x y = error "TODO!"
+sumOfSquares :: (Num a) => a -> a -> a
+sumOfSquares x y = (x * x) + (y * y)
 
 {- | Implement a function that returns the last digit of a given number.
 
@@ -67,7 +69,8 @@ sumOfSquares x y = error "TODO!"
 
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+lastDigit :: Int -> Int
+lastDigit n = (mod (abs(n)) 10)
 
 {- | Write a function that takes three numbers and returns the
 difference between the biggest number and the smallest one.
@@ -81,7 +84,11 @@ and 1 is the smallest, and 7 - 1 = 6.
 Try to use local variables (either let-in or where) to implement this
 function.
 -}
-minmax x y z = error "TODO"
+minmax :: (Integral a) => a -> a -> a -> a
+minmax x y z =
+    let mx = max (max x y) z
+        mi = min (min x y) z
+    in  abs(mx - mi)
 
 {- | Implement a function that takes a string, start and end positions
 and returns a substring of a given string from the start position to
@@ -98,7 +105,8 @@ start position can be considered as zero (e.g. substring from the
 first character) and negative end position should result in an empty
 string.
 -}
-subString start end str = error "TODO"
+subString :: Int -> Int -> String -> String
+subString start end str = [str!!x | x <- [0..(length str -1)], start<=x && x<= end]
 
 {- | Write a function that takes a String — space separated numbers,
 and finds a sum of the numbers inside this string.
@@ -108,8 +116,33 @@ and finds a sum of the numbers inside this string.
 
 The string contains only spaces and/or numbers.
 -}
-strSum str = error "TODO"
 
+merge :: [String] -> String -> [String]
+merge res word
+    | word=="" = res
+    | otherwise = res ++ [word]
+
+splitImpl :: String -> [String] -> String -> [String]
+splitImpl input res word
+    | input==""  = merge res word
+    | c==' '     = splitImpl other (merge res word) ""
+    | otherwise  = splitImpl other res (word++[c])
+    where c:other = input
+
+charToInt :: Char -> Int
+charToInt c = fst (head (filter (\x -> snd x == c) (zip [-1..9] ('-':['0'..'9']))))
+
+stringToInt :: String -> Int
+stringToInt str
+    | c=='-' = -1 * f other
+    | otherwise = f str
+    where f s = foldl (\x c -> x*10 + (charToInt c)) 0 s
+          c:other = str
+
+strSum :: String -> Int
+strSum str =
+    sum(map stringToInt (split str))
+    where split s = splitImpl s [] ""
 {- | Write a function that takes a number and a list of numbers and
 returns a string, saying how many elements of the list are strictly
 greater than the given number and strictly lower.
@@ -123,4 +156,19 @@ and lower than 6 elements (4, 5, 6, 7, 8 and 9).
 
 🕯 HINT: Use recursion to implement this function.
 -}
-lowerAndGreater n list = error "TODO"
+
+intToString :: Int -> String
+intToString x
+    | x < 10 = [c]
+    | otherwise = (intToString other) ++ [c]
+    where other = div x 10
+          c     = ['0'..'9'] !! (mod x 10)
+
+lowerAndGreater :: Int -> [Int] -> String
+lowerAndGreater n list =
+    intToString(n) ++
+    " is greater than " ++
+    intToString(length(filter (<n) list)) ++
+    " elements and lower than " ++
+    intToString(length(filter (>n) list)) ++
+    " elements"
